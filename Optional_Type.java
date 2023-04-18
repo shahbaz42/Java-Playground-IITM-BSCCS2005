034269da73e0f04076da82fcaac10bc27621d5db
Shahbaz <shahbaz_ali@outlook.in>
2023-04-18 23:32:22 +0530

    Optional Type in java

--- Optional_Type.java
+++ Optional_Type.java
@@ -11,11 +11,15 @@ public class Optional_Type {
         System.out.println(hobby.map(String::toUpperCase).get()); // It will return the value in uppercase
 
         // Creating another Optional object with null value
-        Optional<String> hobby2 = Optional.ofNullable(null);
+        // Optional.ofNullable() method is used to create an Optional object with null value
+        Optional<String> hobby2 = Optional.ofNullable(null); 
         //System.out.println(hobby2.get()); // It will throw an exception
         System.out.println(hobby2.isPresent()); // boolean value is returned
         System.out.println(hobby2.orElse("No value present")); // default value is returned
         System.out.println(hobby2.orElseGet(() -> "Default value")); // default is returned using lambda expression
 
+        // ifPresentOrElse() method
+        Optional<String> hobby3 = Optional.ofNullable(null); 
+        hobby3.ifPresentOrElse(System.out::println, () -> System.out.println("No value present"));
     }
 }
